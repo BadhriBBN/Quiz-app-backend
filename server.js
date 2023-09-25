e1a92d117cbd1ac1b6209859c4162da5f01d9de6
BathriBBN <bbnbadhri@gmail.com>
2023-09-25 21:45:25 +0530

    first-commit

--- server.js
+++ server.js
@@ -1,26 +1,44 @@
 import express from 'express';
 import morgan from 'morgan';
-import cors from'cors';
-import { config } from 'dotenv'
+import cors from 'cors';
+import { config } from 'dotenv';
+import router from './router/route.js';
+import connect from './database/conn.js';
 
-const app = express()
+const app = express();
 
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
 config();
 
-
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res)=>{
+
+app.use('/api', router);
+
+app.get('/', (req, res) => {
+  try {
+    res.json('Get Request');
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+
+connect().then(()=>{
     try {
-        res.json("Get Request")
+        app.listen(port, () => {
+            console.log(`Server  Connected to http://localhost:${port}`);
+          });
+        
     } catch (error) {
-        res.json(error)
+        console.log("Cannot connect to the server");
     }
+
+}).catch(error =>{
+    console.log("Invalid Database Connected");
 })
 
-app.listen(8080, ()=> {
-    console.log(`Server  Connected to http://localhost:${port}`);
-})
+
+
